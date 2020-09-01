a9f7fa7483ae0a0db2bbcbf6072b3fc407a31b70
Dogukan <dogukandogruu@gmail.com>
2020-09-01 18:11:23 +0300

    bypass finished

--- anticheat_bypass.py
+++ anticheat_bypass.py
@@ -9,12 +9,13 @@ from selenium import webdriver
 import time
 import cv2
 import pytesseract
+import pyscreenshot as ImageGrab
 
 
-
-url = "https://10fastfingers.com"
+url = "https://10fastfingers.com/login"
 browser = webdriver.Chrome()
 browser.get(url)
+
 
 cont = ""
 while True:
@@ -24,7 +25,13 @@ while True:
     else:
         time.sleep(1)
 
-        
+startButton = browser.find_element_by_xpath("//*[@id=\"start-btn\"]")
+startButton.click()
+
+time.sleep(1.5)
+img = ImageGrab.grab(bbox=(386, 416, 1115, 585))  # X1,Y1,X2,Y2
+# save image file
+img.save('ac.png')
         
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 img = cv2.imread('ac.png')
@@ -32,8 +39,11 @@ text = pytesseract.image_to_string(img)
 words = text.split()
 inputField = browser.find_element_by_xpath("//*[@id=\"word-input\"]")
 
+inputText = ""
 for word in words:
-    inputField.send_keys(word, " ")
+    inputText = inputText + " " + word
     
+inputField.send_keys(inputText)
+time.sleep(0.2)
 submitButton = browser.find_element_by_xpath("//*[@id=\"submit-anticheat\"]")
 submitButton.click()
